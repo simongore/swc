62d48e7874734919e908fe7e12a18445514812e6
simongore <simonjaygore@simons-air.wireless.yale.internal>
2014-06-20 14:49:52 -0400

    Added second column when generating data.

--- generate_data.sh
+++ generate_data.sh
@@ -1,6 +1,12 @@
 # generate data 1 - 10
+# this is fun
 
 for i in {1..10}; do
-    echo "i=   "
+   let j=i+1
+   echo $i $j >> data1.txt
 done
+
+awk '{print $1, $2^2}' data1.txt > tmp.txt
+mv tmp.txt data1.txt
+
 
